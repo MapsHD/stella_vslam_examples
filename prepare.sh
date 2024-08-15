a207dd2727e8003d085cf27928df75cdcffcc766
mwlasiuk <48164306+mwlasiuk@users.noreply.github.com>
2024-08-15 10:03:45 +0200

    Add prepare.sh script

---
+++ prepare.sh
@@ -0,0 +1,119 @@
+apt update -y
+apt upgrade -y
+apt install -y build-essential pkg-config cmake git wget curl unzip
+apt install -y libatlas-base-dev libsuitesparse-dev
+apt install -y libgtk-3-dev ffmpeg libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libavresample-dev libtbb-dev
+apt install -y gfortran
+apt install -y binutils-dev
+apt install -y libyaml-cpp-dev libgflags-dev sqlite3 libsqlite3-dev
+apt install -y libglm-dev libglfw3-dev libpng-dev libjpeg-dev libeigen3-dev libboost-filesystem-dev libboost-program-options-dev
+apt install -y libglew-dev
+apt install -y autogen autoconf libtool
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt install -y nodejs
+apt install -y libprotobuf-dev protobuf-compiler
+
+cd /tmp
+git clone https://github.com/stella-cv/FBoW.git
+cd FBoW
+mkdir build && cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    ..
+make -j4 && sudo make install
+
+cd /tmp
+git clone https://github.com/RainerKuemmerle/g2o.git
+cd g2o
+git checkout 20230223_git
+mkdir build && cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_UNITTESTS=OFF \
+    -DG2O_USE_CHOLMOD=OFF \
+    -DG2O_USE_CSPARSE=ON \
+    -DG2O_USE_OPENGL=OFF \
+    -DG2O_USE_OPENMP=OFF \
+    -DG2O_BUILD_APPS=OFF \
+    -DG2O_BUILD_EXAMPLES=OFF \
+    -DG2O_BUILD_LINKED_APPS=OFF \
+    ..
+make -j4 && sudo make install
+
+cd /tmp
+git clone https://github.com/bombela/backward-cpp.git
+cd backward-cpp
+git checkout 5ffb2c879ebdbea3bdb8477c671e32b1c984beaa
+mkdir build && cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    ..
+make -j4 && sudo make install
+
+cd /tmp
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+git checkout eab3d3449a33a042b1ee7225e1b8b593b1b21e3e
+mkdir build && cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_PANGOLIN_DEPTHSENSE=OFF \
+    -DBUILD_PANGOLIN_FFMPEG=OFF \
+    -DBUILD_PANGOLIN_LIBDC1394=OFF \
+    -DBUILD_PANGOLIN_LIBJPEG=OFF \
+    -DBUILD_PANGOLIN_LIBOPENEXR=OFF \
+    -DBUILD_PANGOLIN_LIBPNG=OFF \
+    -DBUILD_PANGOLIN_LIBTIFF=OFF \
+    -DBUILD_PANGOLIN_LIBUVC=OFF \
+    -DBUILD_PANGOLIN_LZ4=OFF \
+    -DBUILD_PANGOLIN_OPENNI=OFF \
+    -DBUILD_PANGOLIN_OPENNI2=OFF \
+    -DBUILD_PANGOLIN_PLEORA=OFF \
+    -DBUILD_PANGOLIN_PYTHON=OFF \
+    -DBUILD_PANGOLIN_TELICAM=OFF \
+    -DBUILD_PANGOLIN_UVC_MEDIAFOUNDATION=OFF \
+    -DBUILD_PANGOLIN_V4L=OFF \
+    -DBUILD_PANGOLIN_ZSTD=OFF \
+    ..
+make -j4 && sudo make install
+
+cd /tmp
+git clone https://github.com/koide3/iridescence.git
+cd iridescence
+git checkout 085322e0c949f75b67d24d361784e85ad7f197ab
+git submodule update --init --recursive
+mkdir build && cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ..
+make -j4 && sudo make install
+
+cd /tmp
+git clone https://github.com/shinsumicco/socket.io-client-cpp.git
+cd socket.io-client-cpp
+git submodule init
+git submodule update
+mkdir build && cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DBUILD_UNIT_TESTS=OFF \
+    ..
+make -j4
+sudo make install
+
+wget -q https://github.com/google/protobuf/archive/v3.6.1.tar.gz
+tar xf v3.6.1.tar.gz
+cd protobuf-3.6.1
+./autogen.sh
+./configure \
+    --prefix=/usr/local \
+    --enable-static=no
+make -j4
+sudo make install
